ae9b048f51bbd97a9e98e4f604f3155fcfcf7e2c
dell <assistless@proton.me>
2026-02-14 20:39:32 +0700

    remove second system (cont.)

--- rebuild.sh
+++ rebuild.sh
@@ -1,19 +1,2 @@
 #!/bin/bash
-
-# Detect hostname
-HOSTNAME=$(cat /etc/hostname)
-
-echo "Building NixOS configuration for: $HOSTNAME"
-
-case $HOSTNAME in
-  "dell")
-    sudo nixos-rebuild switch --flake ~/dotfiles#dell
-    ;;
-  "lenovo")
-    sudo nixos-rebuild switch --flake ~/dotfiles#lenovo
-    ;;
-  *)
-    echo "Unknown hostname: $HOSTNAME"
-    exit 1
-    ;;
-esac
+sudo nixos-rebuild switch --flake ~/dotfiles#dell
