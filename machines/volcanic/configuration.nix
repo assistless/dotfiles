ea0c0a37ef75cba2c6b41e4a5f6c373fbbc01d19
demi@ultimatum <assistless@proton.me>
2026-09-08 18:22:49 +0700

    .

--- machines/volcanic/configuration.nix
+++ machines/volcanic/configuration.nix
@@ -62,10 +62,6 @@
     allowedTCPPorts = [ 25565 19132 ];
     allowedUDPPorts = [ 25565 19132 config.services.tailscale.port ];
   };
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  system.copySystemConfiguration = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
